4d3c6d6aecd3f74144209678016cd4026e8dff75
Benedikt Hollerauer <hollibene@gmail.com>
2022-12-08 21:55:42 +0100

    tried to implement twoSum algorithm

--- src/easy/1_TwoSum.scala
+++ src/easy/1_TwoSum.scala
@@ -6,9 +6,7 @@ object `1_TwoSum`:
 
     def twoSum(nums: Array[Int], target: Int): Array[Int] =
         def helper(acc: Int): Array[Int] =
-            nums.flatMap(x =>
-                if(x + nums(acc) == target) Array(acc, nums.indexOf(x))
-                else helper(acc + 1)
-            )
+            if(nums(acc + 1) + nums(acc) == target) Array(acc, nums.indexOf(acc + 1))
+            else helper(acc + 1)
 
         helper(0)
