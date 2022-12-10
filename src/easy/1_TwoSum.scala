76250f0d27187a6e6d5566edb5abfefb7cafd8b0
Benedikt Hollerauer <hollibene@gmail.com>
2022-12-10 07:43:49 +0100

    commit

--- src/easy/1_TwoSum.scala
+++ src/easy/1_TwoSum.scala
@@ -8,8 +8,9 @@ object `1_TwoSum`:
 
     def twoSum(nums: Array[Int], target: Int): Array[Int] =
         nums.flatMap(x =>
-            nums.tail.flatMap(y =>  // the tail just works for the first iteration, because only the first element is removed
-                if(x + y == target) Array(nums.indexOf(x), nums.indexOf(y))
-                else Array(789)
-            ).filter(x => x != 789)
+            nums.drop(nums.indexOf(x))
+                .flatMap(y =>
+                    if(x + y == target) {println(x.toString + "_" + y.toString); Array(nums.indexOf(x), nums.indexOf(y))} // here I think it does´t work, when multiple of the same element are present
+                    else Array(789)
+                ).filter(x => x != 789)
         )
