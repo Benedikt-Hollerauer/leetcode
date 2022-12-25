7fd0df86e69d437ad8b37b10f4b7588d4fdc3300
Benedikt Hollerauer <hollibene@gmail.com>
2022-12-25 21:10:19 +0100

    continued to implement 13_RomanToInteger_Current

--- src/easy/`13_RomanToInteger_Current`.scala
+++ src/easy/`13_RomanToInteger_Current`.scala
@@ -6,16 +6,22 @@ object `13_RomanToInteger_Current`:
 
     @main
     def romanToInt(s: String): Int =
-        s.map(numberChar =>
-            numberChar match
-                case 'I' => 1
-                case 'V' => 5
-                case 'X' => 10
-                case 'L' => 50
-                case 'C' => 100
-                case 'D' => 500
-                case 'M' => 1000
+        val seq = s.map(_ match
+            case 'I' => 1
+            case 'V' => 5
+            case 'X' => 10
+            case 'L' => 50
+            case 'C' => 100
+            case 'D' => 500
+            case 'M' => 1000
         )
 
-    println(romanToInt("MCMXCIV"))  // 1994
+        def helper(acc: Int): Int =
+            if(seq(acc) <= seq(acc + 1)) seq(acc) - seq(acc + 1)
+            else seq(acc) + seq(acc + 1)
 
+        helper(0)
+
+
+
+    println(romanToInt("MCMXCIV"))  // 1994  V-I-C-X-M-C-M
