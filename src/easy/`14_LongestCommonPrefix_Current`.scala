7d258bec2c876f64da44410ac6a09c4098f7af0e
Benedikt Hollerauer <hollibene@gmail.com>
2022-12-28 21:07:03 +0100

    continued to implement 14_LongestCommonPrefix_Current

--- src/easy/`14_LongestCommonPrefix_Current`.scala
+++ src/easy/`14_LongestCommonPrefix_Current`.scala
@@ -4,6 +4,10 @@ package easy
 
 object `14_LongestCommonPrefix_Current`:
 
-    def longestCommonPrefix(strs: Array[String]): String = {
-        ???
-    }
+  def longestCommonPrefix(strs: Array[String]): String = {
+      strs.toList
+          .map(x => x.toList)
+
+      // stash the elements of the index in the same sub list like that:
+      // List(List(a, b, c, d), List(a, c, d, e), List(u, b, d, e)) => List(List(a, a, u) <- the first elements of the other arrays, List(b, c, b) <-  the second elements of the other arrays, ...)
+  }
