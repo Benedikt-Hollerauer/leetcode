1a856993a7da11c043528a87f9799008884f7c45
Benedikt Hollerauer <hollibene@gmail.com>
2022-12-30 21:01:31 +0100

    continued to implement 14_LongestCommonPrefix_Current

--- src/easy/`14_LongestCommonPrefix_Current`.scala
+++ src/easy/`14_LongestCommonPrefix_Current`.scala
@@ -4,10 +4,20 @@ package easy
 
 object `14_LongestCommonPrefix_Current`:
 
-  def longestCommonPrefix(strs: Array[String]): String = {
-      strs.toList
-          .map(x => x.toList)
+    def main(args: Array[String]): Unit = {
 
-      // stash the elements of the index in the same sub list like that:
-      // List(List(a, b, c, d), List(a, c, d, e), List(u, b, d, e)) => List(List(a, a, u) <- the first elements of the other arrays, List(b, c, b) <-  the second elements of the other arrays, ...)
-  }
+        longestCommonPrefix(Array("flower", "flight", "flow"))
+    }
+
+    def longestCommonPrefix(strs: Array[String]) = {
+        strs.toList
+            .map(x => x.toList) // List[List[String]]
+
+        def helper(it: List[List[Char]], moin: Option[Char]) =
+            it.reduce((x, y) =>
+                if(x.head == y.head) helper(it.filter(!x.head), Some(x))
+            )
+
+        // stash the elements of the index in the same sub list like that:
+        // List(List(a, b, c, d), List(a, c, d, e), List(u, b, d, e)) => List(List(a, a, u) <- the first elements of the other arrays, List(b, c, b) <-  the second elements of the other arrays, ...)
+    }
