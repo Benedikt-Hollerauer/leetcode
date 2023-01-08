13c46300922204c1e1ff47489d92b74d847f9989
Benedikt Hollerauer <hollibene@gmail.com>
2023-01-08 16:23:10 +0100

    added _21_MergeTwoSortedLists_Current

---
+++ src/easy/_21_MergeTwoSortedLists_Current.scala
@@ -0,0 +1,14 @@
+package easy
+
+// https://leetcode.com/problems/merge-two-sorted-lists/
+
+object _21_MergeTwoSortedLists_Current:
+
+    class ListNode(_x: Int = 0, _next: ListNode = null) {
+        var next: ListNode = _next
+        var x: Int = _x
+    }
+
+    def mergeTwoLists(list1: ListNode, list2: ListNode): ListNode = {
+        ???
+    }
