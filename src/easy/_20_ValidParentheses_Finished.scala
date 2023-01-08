b528116e36a702ae91bd51c90ce92ac28eaf4b39
Benedikt Hollerauer <hollibene@gmail.com>
2023-01-08 16:16:01 +0100

    implemented _20_ValidParentheses_Finished

---
+++ src/easy/_20_ValidParentheses_Finished.scala
@@ -0,0 +1,25 @@
+package easy
+
+import scala.annotation.tailrec
+
+// https://leetcode.com/problems/valid-parentheses/
+
+object _20_ValidParentheses_Finished:
+
+    def isValid(s: String): Boolean = {
+        @tailrec
+        def helper(it: List[Char], stack: List[Char]): Boolean =
+            it match {
+                case Nil if stack.isEmpty => true
+                case ')' :: t if stack.headOption == Some('(') => helper(t, stack.tail)
+                case '}' :: t if stack.headOption == Some('{') => helper(t, stack.tail)
+                case ']' :: t if stack.headOption == Some('[') => helper(t, stack.tail)
+                case h :: t => helper(t , h :: stack)
+                case Nil => false
+            }
+
+        helper(
+            s.toList,
+            List.empty[Char]
+        )
+    }
