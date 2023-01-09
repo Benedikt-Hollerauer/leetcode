7ec0f23a76cc681e069d06503838b134062580b0
Benedikt Hollerauer <hollibene@gmail.com>
2023-01-09 07:20:36 +0100

    continued to implement _21_MergeTwoSortedLists_Current

--- src/easy/_21_MergeTwoSortedLists_Current.scala
+++ src/easy/_21_MergeTwoSortedLists_Current.scala
@@ -2,7 +2,7 @@ package easy
 
 // https://leetcode.com/problems/merge-two-sorted-lists/
 
-object _21_MergeTwoSortedLists_Current:
+object _21_MergeTwoSortedLists_Current extends App:
 
     class ListNode(_x: Int = 0, _next: ListNode = null) {
         var next: ListNode = _next
@@ -10,5 +10,22 @@ object _21_MergeTwoSortedLists_Current:
     }
 
     def mergeTwoLists(list1: ListNode, list2: ListNode): ListNode = {
-        ???
+        def helper(list1: ListNode, list2: ListNode, intermediate: List[Int]): ListNode =
+            if(list1.next == null && list2.next == null) {
+                intermediate
+                    .sorted
+                    .map(x => new ListNode(x))
+                    .reduce((x, y) => new ListNode(x.x, y))
+            }
+            else if(list1.next != null) helper(list1.next, list2, list1.x :: intermediate)
+            else helper(list1, list2.next, list2.x :: intermediate)
+
+        helper(list1, list2, List.empty[Int])
     }
+
+    println(
+        mergeTwoLists(
+            new ListNode(1, new ListNode(2, new ListNode(4))),
+            new ListNode(1, new ListNode(3, new ListNode(4)))
+        ).next.next.x
+    )
