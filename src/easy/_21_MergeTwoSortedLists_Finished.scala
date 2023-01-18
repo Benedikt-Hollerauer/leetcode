ff3b4f3951452876bf576060e640ca983e7dd399
Benedikt Hollerauer <hollibene@gmail.com>
2023-01-18 07:23:49 +0100

    added _26_RemoveDuplicatesFromSortedArray_Current

--- src/easy/_21_MergeTwoSortedLists_Finished.scala
+++ src/easy/_21_MergeTwoSortedLists_Finished.scala
@@ -1,5 +1,7 @@
 package easy
 
+import scala.annotation.tailrec
+
 // https://leetcode.com/problems/merge-two-sorted-lists/
 
 object _21_MergeTwoSortedLists_Finished:
@@ -10,6 +12,7 @@ object _21_MergeTwoSortedLists_Finished:
     }
 
     def mergeTwoLists(list1: ListNode, list2: ListNode): ListNode = {
+        @tailrec
         def helper(list1: ListNode, list2: ListNode, acc: List[Int]): ListNode =
             if(list1 != null) helper(list1.next, list2, list1.x :: acc)
             else if(list2 != null) helper(list1, list2.next, list2.x :: acc)
---
+++ src/easy/_26_RemoveDuplicatesFromSortedArray_Current.scala
@@ -0,0 +1,7 @@
+package easy
+
+object _26_RemoveDuplicatesFromSortedArray_Current:
+
+    def removeDuplicates(nums: Array[Int]): Int = {
+        ???
+    }
