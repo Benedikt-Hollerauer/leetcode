75545a4ffb21f59c987dd4d5740f4152063470c0
Benedikt Hollerauer <hollibene@gmail.com>
2023-01-25 21:55:53 +0100

    finished _35_SearchInsertPosition_Finished

--- src/easy/_35_SearchInsertPosition_Finished.scala
+++ src/easy/_35_SearchInsertPosition_Finished.scala
@@ -2,8 +2,8 @@ package easy
 
 // https://leetcode.com/problems/search-insert-position/
 
-object _35_SearchInsertPosition_Current:
+object _35_SearchInsertPosition_Finished:
 
     def searchInsert(nums: Array[Int], target: Int): Int = {
-        ???
+        nums.filter(_ < target).length
     }
