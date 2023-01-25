8b2c51c10a19dfef81a9da2e37b130f374a1411b
Benedikt Hollerauer <hollibene@gmail.com>
2023-01-25 21:16:00 +0100

    added _35_SearchInsertPosition_Current

---
+++ src/easy/_35_SearchInsertPosition_Current.scala
@@ -0,0 +1,9 @@
+package easy
+
+// https://leetcode.com/problems/search-insert-position/
+
+object _35_SearchInsertPosition_Current:
+
+    def searchInsert(nums: Array[Int], target: Int): Int = {
+        ???
+    }
