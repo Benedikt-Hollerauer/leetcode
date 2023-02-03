4c4b9d316469f9782008c4537f4239c3c81f03f4
Benedikt Hollerauer <hollibene@gmail.com>
2023-02-03 19:46:46 +0100

    continued to implement _1_TwoSum_U and _67_AddBinary_C

--- src/easy/_1_TwoSum_U.scala
+++ src/easy/_1_TwoSum_U.scala
@@ -2,17 +2,17 @@ package easy
 
 // https://leetcode.com/problems/two-sum/
 
-object _1_TwoSum_U:
+object _1_TwoSum_U extends App:
 
     def twoSum(nums: Array[Int], target: Int): Array[Int] = {
-        nums.flatMap(x =>
-            nums.drop(nums.indexOf(x))
-                .flatMap(y =>
-                    if(x + y == target) {
-                        println(x.toString + "_" + y.toString)
-                        Array(nums.indexOf(x), nums.indexOf(y))
-                    } // here I think it does´t work, when multiple of the same element are present
-                    else Array(999)
-                ).filter(x => x != 999)
-        )
+        nums.zipWithIndex
+            .combinations(2)
+            .map {
+                case Array(x, y) => (x._2, y._2, x._1 + y._1)
+            }.filter(x => x._3 == target)
+            .flatMap  {
+                case (x, y, z) => Array(x, y)
+            }.toArray
     }
+
+    twoSum(Array(2, 7, 11, 15), 9).foreach(println)
--- src/easy/_67_AddBinary_C.scala
+++ src/easy/_67_AddBinary_C.scala
@@ -6,6 +6,7 @@ object _67_AddBinary_C extends App:
 
     def addBinary(a: String, b: String) = {
         val it = List(a, b)
+        println(
             it.map(
                 _.reverse
                     .padTo(it.max.length, "0")
@@ -13,7 +14,7 @@ object _67_AddBinary_C extends App:
             ).transpose
                 .reverse
                 .map(_.map(_.toString.toInt).sum)
-                .foreach(println)
+        )
     }
 
     addBinary(
