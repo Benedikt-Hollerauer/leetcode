8af0bd8084e2335628f43f5819fd21316afbfee1
Benedikt Hollerauer <hollibene@gmail.com>
2023-02-06 07:21:22 +0100

    continued to implement _67_AddBinary_C

--- src/easy/_67_AddBinary_C.scala
+++ src/easy/_67_AddBinary_C.scala
@@ -6,20 +6,23 @@ object _67_AddBinary_C extends App:
 
     def addBinary(a: String, b: String) = {
         val it = List(a, b)
-        println(
-            it.map(
-                _.reverse
-                    .padTo(it.max.length, "0")
-                    .reverse
-            ).transpose
+        it.map(
+            _.reverse
+                .padTo(it.max.length, "0")
                 .reverse
-                .map(_.map(_.toString.toInt).sum)
-        )
+        ).transpose
+            .reverse
+            .map(_.map(_.toString.toInt).sum.toString)
+            .appended("0")
+            .reduce {
+                case (x ,y) if x.takeRight(1) == "2" => x.dropRight(1) + "0" + (y.toInt + 1).toString
+                case (x, y) => x + y
+            }.reverse
     }
 
     addBinary(
-        "1010",
-        "1011"
+        "11",
+        "1"
     )
 
     // should be: 10101
