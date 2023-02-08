59dfcb2b265a0806f8c10a1b5aab037f7d993d3b
Benedikt Hollerauer <88812164+Benedikt-Hollerauer@users.noreply.github.com>
2023-02-08 12:36:35 +0100

    refactored _67_AddBinary_F

--- src/easy/_67_AddBinary_F.scala
+++ src/easy/_67_AddBinary_F.scala
@@ -11,11 +11,11 @@ object _67_AddBinary_F:
         ).transpose
             .map(_.map(_.toString.toInt).sum.toString)
             .appended("0")
-            .reduce { (x, y) => (x, y) match {
+            .reduce((x, y) => (x, y) match {
                 case (x, y) if x.takeRight(1) == "3" => x.dropRight(1) + "1" + (y.toInt + 1).toString
                 case (x, y) if x.takeRight(1) == "2" => x.dropRight(1) + "0" + (y.toInt + 1).toString
                 case (x, y) => x + y
-            }}.reverse.toList match {
+            }).reverse.toList match {
                 case '0' :: t => t.mkString
                 case s => s.mkString
             }
