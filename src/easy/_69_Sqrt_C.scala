2280d4b07004ca078f36d221e2b79f0942ccae6b
Benedikt Hollerauer <hollibene@gmail.com>
2023-02-12 09:04:46 +0100

    _69_Sqrt_C

---
+++ src/easy/_69_Sqrt_C.scala
@@ -0,0 +1,15 @@
+package easy
+
+/**
+ *  https://leetcode.com/problems/sqrtx/
+ *
+ *  Time: O()
+ *
+ *  Space: O()
+ */
+
+object _69_Sqrt_C:
+
+    def mySqrt(x: Int): Int = {
+        ???
+    }
