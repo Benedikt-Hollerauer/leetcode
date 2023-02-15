2639dd61d719e01b9262c59e23642c60a1475ba2
Benedikt Hollerauer <hollibene@gmail.com>
2023-02-15 18:54:39 +0100

    added _2_AddTwoNumbers_C

---
+++ src/medium/_2_AddTwoNumbers_C.scala
@@ -0,0 +1,12 @@
+package medium
+
+object _2_AddTwoNumbers_C:
+
+    class ListNode(_x: Int = 0, _next: ListNode = null) {
+        var next: ListNode = _next
+        var x: Int = _x
+    }
+
+    def addTwoNumbers(l1: ListNode, l2: ListNode): ListNode = {
+        ???
+    }
