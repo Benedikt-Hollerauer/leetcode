324c79acdd1a298d920af2ad211331bd05cf3d5a
Benedikt Hollerauer <hollibene@gmail.com>
2023-02-17 08:51:06 +0100

    added _3_LongestSubstringWithoutRepeatingCharacters_C

---
+++ src/medium/_3_LongestSubstringWithoutRepeatingCharacters_C.scala
@@ -0,0 +1,15 @@
+package medium
+
+/**
+ * https://leetcode.com/problems/longest-substring-without-repeating-characters/
+ *
+ * Time: O()
+ *
+ * Space: O()
+ */
+
+object _3_LongestSubstringWithoutRepeatingCharacters_C:
+
+    def lengthOfLongestSubstring(s: String): Int = {
+        ???
+    }
