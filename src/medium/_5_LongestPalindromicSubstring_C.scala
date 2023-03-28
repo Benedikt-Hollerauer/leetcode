1ecd8f8a4c3237fdc2ef6b62688130efac4506bd
Benedikt Hollerauer <hollibene@gmail.com>
2023-03-28 06:59:03 +0200

    added _5_LongestPalindromicSubstring_C

---
+++ src/medium/_5_LongestPalindromicSubstring_C.scala
@@ -0,0 +1,21 @@
+package medium
+
+/**
+ * https://leetcode.com/problems/longest-palindromic-substring/
+ *
+ * Time: O()
+ *
+ * Space: O()
+ */
+
+object _5_LongestPalindromicSubstring_C extends App:
+
+    def longestPalindrome(s: String): String = {
+        ???
+    }
+
+    List(
+        "babad",
+        "cbbd",
+    ).map(longestPalindrome)
+        .foreach(println)
