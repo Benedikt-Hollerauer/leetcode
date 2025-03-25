0550ea6195af74739e47fa93a7c2a07a7a706501
Benedikt Hollerauer <benedikt.hollerauer@braintower.de>
2025-03-25 12:37:52 +0100

    added two sum in rust

---
+++ rust/src/main.rs
@@ -0,0 +1,31 @@
+fn main() {
+    println!("{:?}", two_sum(vec![3,2,4], 6));
+}
+
+pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    let test = nums
+        .iter()
+        .enumerate()
+        .map(|(index_outer, value_outer)|
+            nums
+                .iter()
+                .enumerate()
+                .fold(
+                    None,
+                    |acc, (index_inner, value_inner)| {
+                        return if value_inner + value_outer == target {
+                            Some(vec![index_inner as i32, index_outer as i32])
+                        } else if acc.is_some() {
+                            acc
+                        } else {
+                            None
+                        }
+                    }
+                )
+        );
+    println!("{:?}", test.clone().collect::<Vec<_>>());
+    test.filter(|option| option.is_some())
+        .take(1)
+        .flat_map(|option| option.unwrap())
+        .collect::<Vec<_>>()
+}
