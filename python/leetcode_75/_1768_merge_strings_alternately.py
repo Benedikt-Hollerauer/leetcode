3a2ed089a3b79b2c57bbb540a4c1322b330b784d
Benedikt Hollerauer <benedikt.hollerauer@braintower.de>
2025-08-25 12:15:17 +0200

    started implementing 1768

--- python/leetcode_75/_1768_merge_strings_alternately.py
+++ python/leetcode_75/_1768_merge_strings_alternately.py
@@ -1,3 +1,9 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        
+        final_world_list = []
+        for letter_word1 in word1:
+            for letter_word2 in word2:
+                
+        return "".join(final_world_list)
+
+Solution().mergeAlternately("abc", "pqr")
