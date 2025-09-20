0a726aad761c28028f419f3620af199959256b62
Benedikt Hollerauer <benedikt.hollerauer@braintower.de>
2025-09-20 14:21:29 +0200

    adjusted code to run some test cases

--- python/leetcode_75/_1768_merge_strings_alternately.py
+++ python/leetcode_75/_1768_merge_strings_alternately.py
@@ -1,9 +1,19 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        final_world_list = []
-        for letter_word1 in word1:
-            for letter_word2 in word2:
-                
-        return "".join(final_world_list)
+        merged_alternately_result = ""
+        if len(word1) > len(word2):
+            for i in range(0, len(word1)):
+                if len(word2) -1 < i:
+                    merged_alternately_result = merged_alternately_result + word1[i]
+                else:
+                    merged_alternately_result = merged_alternately_result + word1[i] + word2[i]
+        else:
+            for i in range(0, len(word1)):
+                if len(word1) -1 < i:
+                    merged_alternately_result = merged_alternately_result + word2[i]
+                else:
+                    merged_alternately_result = merged_alternately_result + word1[i] + word2[i]
+        return merged_alternately_result
 
-Solution().mergeAlternately("abc", "pqr")
+result = Solution().mergeAlternately("abcd", "pq")
+print(result)
